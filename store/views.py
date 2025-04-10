41db27bf87ba94237642d9ba1570eea6d13dda4d
Sohib <abdumanobovsohib13@gmail.com>
2025-04-10 18:15:28 +0500

    O'yinlar databazasini qo'shdim

--- store/views.py
+++ store/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Games
 
 # Create your views here.
 def index(request):
-    return render(request, 'store/index.html')
+    games = Games.objects.all()
+    context = {
+        'games':games,
+        'title':"Zteam"
+    }
+    return render(request, 'store/index.html', context)
 
 def browse(request):
     return render(request, 'store/browse.html')
